095d7aa76254a81f0b125c52af6c527f37909ee3
RummanAnsari <rummanansari6@gmail.com>
2023-03-09 10:53:47 +0530

    Solution Done

--- Example 04 - Input two numbers from the user and display their sum and product.py
+++ Example 04 - Input two numbers from the user and display their sum and product.py
@@ -1,3 +1,16 @@
 '''
 Example 4: Input two numbers from the user and display their sum and product.
 '''
+
+
+# Input two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate their sum and product
+sum = num1 + num2
+product = num1 * num2
+
+# Display the results
+print("The sum of", num1, "and", num2, "is", sum)
+print("The product of", num1, "and", num2, "is", product)

4ba13c1b4e39044adbef040a63a86e9874427a82
RummanAnsari <rummanansari6@gmail.com>
2023-03-09 10:46:56 +0530

    Solution Done

--- Example 03 - Display a welcome message to the user.py
+++ Example 03 - Display a welcome message to the user.py
@@ -1,3 +1,10 @@
-'''
-Example 3: Display a welcome message to the user.
-'''
+#Example 3: Display a welcome message to the user.
+
+print("hi")
+
+user_name = input("Enter your name: ")
+print("welcome ", user_name)
+
+
+
+

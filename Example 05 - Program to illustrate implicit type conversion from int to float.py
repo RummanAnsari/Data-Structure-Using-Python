9a7068e9d0fb09ad116a1f12cd9a113b94ad16f8
RummanAnsari <rummanansari6@gmail.com>
2023-03-09 10:58:40 +0530

    Solution Done

--- Example 05 - Program to illustrate implicit type conversion from int to float.py
+++ Example 05 - Program to illustrate implicit type conversion from int to float.py
@@ -2,3 +2,12 @@
 Example 5: Program to illustrate implicit type conversion from int to float.
 '''
 
+# Define an integer value
+num_int = 10
+
+# Convert the integer value to float
+num_float = num_int + 12.0
+
+# Print both values with their data types
+print("The integer value is:", num_int, "and its data type is:", type(num_int))
+print("The float value is:", num_float, "and its data type is:", type(num_float))

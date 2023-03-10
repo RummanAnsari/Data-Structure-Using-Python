80f5aeab35f8a216aa2cf185854b768e4a686ea8
RummanAnsari <rummanansari6@gmail.com>
2023-03-10 15:31:49 +0530

    Solution Added

---
+++ Example 07 - Print Function.py
@@ -0,0 +1,39 @@
+# 1. Printing a string:
+print("Hello, World!") # Hello, World!
+
+# 2. Printing a variable:
+name = "John"
+print("My name is", name) # My name is John
+
+
+# 3. Printing a number:
+age = 25
+print("I am", age, "years old.") # I am 25 years old.
+
+
+# 4. Printing a formatted string using f-string:
+name = "Mary"
+age = 32
+print(f"My name is {name} and I am {age} years old.") 
+# My name is Mary and I am 32 years old.
+
+
+# 5. Printing multiple items separated by a specific character:
+item1 = "apple"
+item2 = "banana"
+item3 = "orange"
+print(item1, item2, item3, sep=", ") # apple, banana, orange
+
+
+# 6. Printing without a newline character:
+print("Hello,", end=" ")
+print("World!") # Hello, World!
+
+
+
+
+
+
+
+
+

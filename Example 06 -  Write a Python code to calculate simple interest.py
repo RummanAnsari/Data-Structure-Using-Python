7ce92e1aee2720330b10d20ac7e627c27da6bc66
RummanAnsari <rummanansari6@gmail.com>
2023-03-10 15:25:38 +0530

    Solution Added

--- Example 06 -  Write a Python code to calculate simple interest.py
+++ Example 06 -  Write a Python code to calculate simple interest.py
@@ -3,3 +3,26 @@ Example 6: Write a Python code to calculate simple interest and amount payable b
 
 (Formula used: Simple Interest = Principal * Rate * Time/100)
 '''
+
+# Taking input from the user
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+
+# Time period is fixed at 5 years
+time = 5
+
+# Calculating simple interest and amount payable
+simple_interest = (principal * rate * time) / 100
+amount_payable = principal + simple_interest
+
+# Displaying the result
+print("Simple Interest = ", simple_interest)
+print("Amount Payable = ", amount_payable)
+
+
+# Output
+
+# Enter the principal amount: 10000
+# Enter the rate of interest: 2
+# Simple Interest =  1000.0
+# Amount Payable =  11000.0

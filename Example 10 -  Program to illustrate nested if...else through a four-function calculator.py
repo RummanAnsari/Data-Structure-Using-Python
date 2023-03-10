1a990562e722ce0a28ffaf9a86def63227f4a1d3
RummanAnsari <rummanansari6@gmail.com>
2023-03-10 15:50:00 +0530

    Solution Done

--- Example 10 -  Program to illustrate nested if...else through a four-function calculator.py
+++ Example 10 -  Program to illustrate nested if...else through a four-function calculator.py
@@ -1,3 +1,30 @@
 '''
 Example 10: Program to illustrate nested if...else through a four-function calculator.
 '''
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+# Performing the calculation based on the operator
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    # Handling division by zero
+    if num2 == 0:
+        print("Error: Cannot divide by zero")
+    else:
+        result = num1 / num2
+else:
+    print("Error: Invalid operator")
+
+# Displaying the result
+if operator == '/' and num2 == 0:
+    pass  # Do nothing
+else:
+    print(num1, operator, num2, "=", result)

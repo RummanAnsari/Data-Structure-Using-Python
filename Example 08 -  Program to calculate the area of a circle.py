52d2d41ce88595d75748ac23087485f774dca132
RummanAnsari <rummanansari6@gmail.com>
2023-03-10 15:40:26 +0530

    Solution Added

--- Example 08 -  Program to calculate the area of a circle.py
+++ Example 08 -  Program to calculate the area of a circle.py
@@ -1,3 +1,12 @@
 '''
 Example 8: Program to calculate the area of a circle.
 '''
+
+# Taking input from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculating area of the circle
+area = 3.14 * radius * radius
+
+# Displaying the result
+print("The area of the circle is: ", area)
